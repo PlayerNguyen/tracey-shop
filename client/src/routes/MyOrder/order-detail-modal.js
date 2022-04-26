c3d698ab15dc033da817c0fcf1ed5509bcefb421
TungNWS <100254840+TungNWS@users.noreply.github.com>
2022-04-26 18:15:45 +0700

    update

--- client/src/routes/MyOrder/order-detail-modal.js
+++ client/src/routes/MyOrder/order-detail-modal.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Modal from "../../components/Modal";
+import React from 'react';
+import Modal from '../../components/Modal';
 
 function OrderDetailModal({ open, onClose, orderDetail }) {
-    return (
-        <>
-            <Modal dimmer={true} onClose={onClose} open={open}>
-              
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Modal dimmer={true} onClose={onClose} open={open}>
+          <p className='text-xl font-semibold'>Thông tin đơn hàng</p>
+      </Modal>
+    </>
+  );
 }
 
 export default OrderDetailModal;
