dfeafecdbe7a9f0870cadc259024160366f1b87c
ZaffranLe <biprono12@gmail.com>
2022-05-04 00:18:08 +0700

    update item build list

--- client/src/routes/Build/index.js
+++ client/src/routes/Build/index.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React from 'react';
 
 function Build() {
-  return <>
-    <div className="bg-white grid grid-cols-6 gap-x-4">
-      <div className="border border-gray-500 p-2 rounded-lg">
-        Test
+  const DEFAULT_ITEMS = [
+    {
+      category: 'Vi xử lý',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'Bo mạch chủ',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'RAM',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'HDD',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'SSD',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'VGA',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'Nguồn',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'Vỏ case',
+      product: null,
+      quantity: 0,
+    },
+    {
+      category: 'Màn hình',
+      product: null,
+      quantity: 0,
+    },
+  ];
+  const [items, setItems] = React.useState(DEFAULT_ITEMS);
+
+  return (
+    <>
+      <div className="bg-white grid grid-cols-6 gap-x-4">
+        <table className="table-fixed w-full">
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>
+                  {index + 1}. {item.category}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <div className="col-span-5 p-2">
-        Test
-      </div>
-    </div>
-  </>
+    </>
+  );
 }
 
 export default Build;
